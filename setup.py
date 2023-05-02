67e63503f5c89975d8bc572899a09c49ed975601
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2023-05-02 22:22:01 +0300

    fichier pour la génération de l'exécutable
    Ajout du fichier setup.py pour la génération de l'exécutable avec cx_Freeze

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Chemin vers l'icône
-icon_path = "issets\\icon.ico"
+icon_path = "assets\\icon.ico"
 
 # Nom de votre fichier exécutable
 exe_name = "Play 2048"
 
 # Paramètres de configuration de Cx_Freeze
-build_exe_options = {"packages": ["pygame"], "excludes": ['PyQt6', 'scipy', 'numpy', 'jupyter', 'altgraph', 'asyncio', 'cffi', 'concurrent', 'ctypes', 'distutils', 'email', 'html', 'http', 'jinja2', 'json', 'lib2to3', 'logging', 'markupsafe', 'multiprocessing', 'ordlookup', 'packaging', 'pkg_resources', 'pycparser', 'pydoc_data', 'PyInstaller', 'pyparsing', 'pywin32_system32', 'setuptools', 'tcl8', 'test', 'tk8.6', 'tkinter', 'unittest', 'urillib', 'win32com', 'win32ctypes', 'xml', 'xmlrpc', 'zipp'], "include_files": [icon_path, 'requirements.txt', 'high_score.txt', 'issets']}
+build_exe_options = {"packages": ["pygame"], "excludes": ['PyQt6', 'scipy', 'numpy', 'jupyter', 'altgraph', 'asyncio', 'cffi', 'concurrent', 'ctypes', 'distutils', 'email', 'html', 'http', 'jinja2', 'json', 'lib2to3', 'logging', 'markupsafe', 'multiprocessing', 'ordlookup', 'packaging', 'pkg_resources', 'pycparser', 'pydoc_data', 'PyInstaller', 'pyparsing', 'pywin32_system32', 'setuptools', 'tcl8', 'test', 'tk8.6', 'tkinter', 'unittest', 'urillib', 'win32com', 'win32ctypes', 'xml', 'xmlrpc', 'zipp'], "include_files": [icon_path, 'requirements.txt', 'high_score.txt', 'assets']}
 
 # Création de l'exécutable
 base = None
