2c908d25d0ea66d935a7abda08d3389e2ede886a
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2023-05-02 22:02:09 +0300

    encapsuler game_logic et gui dans une classe
    mettre le jeu dans une classe pour lisibilité et propreté du code

--- main.py
+++ main.py
@@ -1,252 +1,275 @@
 import pygame
 import random
+from game_constants import COLORS
 
-pygame.init()
+# size of the grid
+N = 4
 
-icon = pygame.image.load('issets\\2048.png')
-pygame.display.set_icon(icon)
+class Game_2048:
+    def __init__(self):
+        # Initialization
+        pygame.init()
+        pygame.font.init()
+        pygame.display.set_caption('2048')
 
-# initial set up
-WIDTH = 400
-HEIGHT = 500
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption('2048')
-timer = pygame.time.Clock()
-fps = 60
+        # Variables
+        self.grid = [[0 for _ in range(N)] for _ in range(N)]
+        self.is_over = False
+        self.moved = False
+        self.initial_high_score = 0
+        self.score = 0
+        self.high_score = self.find_high_score()
+        self.width = 400
+        self.height = 500
+        self.spacecing = 10
+        self.screen = pygame.display.set_mode([self.width, self.height])
+        self.screen.fill(pygame.Color('gray'))
 
-# 2048 game color library
-colors = {
-    0: (204, 192, 179),
-    2: (238, 228, 218),
-    4: (237, 224, 200),
-    8: (242, 177, 121),
-    16: (245, 149, 99),
-    32: (246, 124, 95),
-    64: (246, 94, 59),
-    128: (237, 207, 114),
-    256: (237, 204, 97),
-    512: (237, 200, 80),
-    1024: (237, 197, 63),
-    2048: (237, 194, 46),
-    'light text': (249, 246, 242),
-    'dark text': (119, 110, 101),
-    'other': (0, 0, 0),
-    'bg': (187, 173, 160)
-}
+        # time
+        self.time = pygame.time.Clock()
 
-# game variables initialize
-board_values = [[0 for _ in range(4)] for _ in range(4)]
-font = pygame.font.Font('freesansbold.ttf', 24)
-spawn_new = True
-direction = ''
-score = 0
-file_high_score = open('high_score.txt', 'r')
-init_high = int(file_high_score.readline())
-file_high_score.close()
-high_score = init_high
-first_time = True
-UP, DOWN, RIGHT, LEFT = 0, 1, 2, 3
-
-# draw game over and restart text
-def draw_over():
-    pygame.draw.rect(screen, 'black', [50, 50, 300, 100], 0, 10)
-    game_over_text1 = font.render('GAME OVER !', True, 'white')
-    game_over_text2 = font.render('Press ENTER to restart', True, 'white')
-    screen.blit(game_over_text1, (130, 65))
-    screen.blit(game_over_text2, (70, 105))
-
-# Take your turn based on direction
-def take_turn(direc, board):
-    global score
-    merged = [[False for _ in range(4)] for _ in range(4)]
-    can_move = [0, 0, 0, 0]
-
-    if direc==UP:
-        for i in range(4):
-            for j in range(4):
-                shift = 0
-                if i>0 and board[i][j]:
-                    for q in range(i):
-                        if board[q][j]==0:
-                            shift += 1
-                    if shift>0:
-                        board[i-shift][j] = board[i][j]
-                        board[i][j] = 0
-                        can_move[direc] = True
-                    if i-shift-1>=0 and board[i-shift-1][j]==board[i-shift][j] and not merged[i-shift-1][j]:
-                        board[i-shift-1][j] *= 2
-                        score += board[i-shift-1][j]
-                        board[i-shift][j] = 0
-                        merged[i-shift-1][j] = True
-                        can_move[direc] = True
-    elif direc==DOWN:
-        for i in reversed(range(4)):
-            for j in range(4):
-                shift = 0
-                if i<3 and board[i][j]:
-                    for q in range(i+1, 4):
-                        if board[q][j]==0:
-                            shift += 1
-                    if shift>0:
-                        board[i+shift][j] = board[i][j]
-                        board[i][j] = 0
-                        can_move[direc] = True
-                    if i+shift+1<=3 and board[i+shift+1][j]==board[i+shift][j] and not merged[i+shift+1][j]:
-                        board[i+shift+1][j] *= 2
-                        score += board[i+shift+1][j]
-                        board[i+shift][j] = 0
-                        merged[i+shift+1][j] = True
-                        can_move[direc] = True
-    elif direc==LEFT:
-        for i in range(4):
-            for j in range(4):
-                shift = 0
-                if j>0 and board[i][j]:
-                    for q in range(j):
-                        if board[i][q]==0:
-                            shift += 1
-                    if shift>0:
-                        board[i][j-shift] = board[i][j]
-                        board[i][j] = 0
-                        can_move[direc] = True
-                    if j-shift-1>=0 and board[i][j-shift-1]==board[i][j-shift] and not merged[i][j-shift-1]:
-                        board[i][j-shift-1] *= 2
-                        score += board[i][j-shift-1]
-                        board[i][j-shift] = 0
-                        merged[i][j-shift-1] = True
-                        can_move[direc] = True
-    elif direc==RIGHT:
-        for i in range(4):
-            for j in reversed(range(4)):
-                shift = 0
-                if j<3 and board[i][j]:
-                    for q in range(j+1, 4):
-                        if board[i][q]==0:
-                            shift += 1
-                    if shift>0:
-                        board[i][j+shift] = board[i][j]
-                        board[i][j] = 0
-                        can_move[direc] = True
-                    if j+shift+1<=3 and board[i][j+shift+1]==board[i][j+shift] and not merged[i][j+shift+1]:
-                        board[i][j+shift+1] *= 2
-                        score += board[i][j+shift+1]
-                        board[i][j+shift] = 0
-                        merged[i][j+shift+1] = True
-                        can_move[direc] = True
+    def find_high_score(self):
+        file_high_score = open('high_score.txt', 'r')
+        self.initial_high_score = int(file_high_score.readline())
+        file_high_score.close()
+        return self.initial_high_score
     
-    return board, can_move[direc]
+    def draw_score(self):
+        """Draw the score on the screen"""
 
-# detect if no move available => game over
-def is_game_over(board):
-    for i in range(4):
-        for j in range(4):
-            for r, c in [(i+1, j), (i, j+1)]:
-                if 0<=r<4 and 0<=c<4:
-                    if board[i][j]==board[r][c] or board[r][c]==0 or board[i][j]==0:
-                        return False
-    return True
+        # Clear the area where scores are displayed
+        score_rect = pygame.Rect(10, 410, self.width, 40)
+        high_score_rect = pygame.Rect(10, 450, self.width, 40)
+        pygame.draw.rect(self.screen, pygame.Color('gray'), score_rect)
+        pygame.draw.rect(self.screen, pygame.Color('gray'), high_score_rect)
 
-# spawn in new pieces randomly when turns start
-def new_pieces(board):
-    global first_time
-    cnt_new_tiles = 2 if first_time else 1
-    first_time = False
+        font = pygame.font.Font('freesansbold.ttf', 24)
+        score_text = font.render(f'Score: {self.score}', True, 'black')
+        high_score_text = font.render(f'High Score: {self.high_score}', True, 'black')
+        self.screen.blit(score_text, (10, 410))
+        self.screen.blit(high_score_text, (10, 450))
+        pygame.display.flip()
+        
+    def is_game_over(self):
+        """Chech if the player lose. He can't move any of 4 directions"""
 
-    while any(0 in row for row in board) and cnt_new_tiles:
-        row = random.randint(0, 3)
-        col = random.randint(0, 3)
-        if board[row][col]==0:
-            cnt_new_tiles -= 1
-            if random.randint(1, 10)==10:
-                board[row][col] = 4
+        for i in range(N):
+            for j in range(N):
+                for r, c in [(i+1, j), (i, j+1)]:
+                    if 0<=r<N and 0<=c<N:
+                        if self.grid[r][c]==self.grid[i][j] or self.grid[i][j]==0 or self.grid[r][c]==0:
+                            self.is_over = False
+                            return False
+
+        self.is_over = True
+        return True
+    
+    def draw_game_over(self):
+        """Print Game over on the screen if the user lose"""
+
+        # font
+        font = pygame.font.Font('freesansbold.ttf', 24)
+
+        # rectangle
+        pygame.draw.rect(self.screen, 'black', [50, 50, 300, 100], 0, 10)
+
+        game_over_text1 = font.render('GAME OVER !', True, 'white')
+        game_over_text2 = font.render('Press ENTER to restart', True, 'white')
+
+        self.screen.blit(game_over_text1, (130, 65))
+        self.screen.blit(game_over_text2, (70, 105))
+
+        pygame.display.flip()
+
+    def new_number(self, k=1):
+        """Add number after each user move"""
+
+        empty_tiles = []
+        # get free positions in the grid
+        for i in range(N):
+            for j in range(N):
+                if self.grid[i][j]==0:
+                    empty_tiles.append((i, j))
+
+        # print 2 or 4 in two positions on the empty tiles
+        while k:
+            random.shuffle(empty_tiles)
+            x, y = empty_tiles.pop()
+            if random.random()<0.1:
+                self.grid[x][y] = 4
             else:
-                board[row][col] = 2
+                self.grid[x][y] = 2
+            k -= 1
 
-    return board, is_game_over(board)
+    def make_move(self, move):
+        """Actualise the grid after user move"""
 
-# draw background for the board
-def draw_board():
-    pygame.draw.rect(screen, colors['bg'], [0, 0, 400, 400], 0, 10)
-    score_text = font.render(f'Score: {score}', True, 'black')
-    high_score_text = font.render(f'High Score: {high_score}', True, 'black')
-    screen.blit(score_text, (10, 410))
-    screen.blit(high_score_text, (10, 450))
-    
-def draw_pieces(board):
-    for i in range(4):
-        for j in range(4):
-            value = board[i][j]
-            if value>8:
-                value_color = colors['light text']
-            else:
-                value_color = colors['dark text']
-            if value<=2048:
-                color_tile = colors[value]
-            else:
-                color_tile = colors['other']
-            pygame.draw.rect(screen, color_tile, [j*95 + 20, i*95 + 20, 75, 75], 0, 5)
-            if value>0:
-                value_len = len(str(value))
-                font = pygame.font.Font('freesansbold.ttf', 48 - (5*value_len))
-                value_text = font.render(str(value), True, value_color)
-                text_rect = value_text.get_rect(center=(j*95 + 57, i*95 + 57))
-                screen.blit(value_text, text_rect)
-                pygame.draw.rect(screen, 'black', [j*95 + 20, i*95 + 20, 75, 75], 2, 5)
+        merged = [[False for _ in range(N)] for _ in range(N)]
+        self.grid = self.grid.copy()
 
-# main game loop
-run = True
-while run:
-    timer.tick(fps)
-    screen.fill('gray')
+        if move in {'l', 'r'}:
+            if move=='r':
+                self.grid = [row[::-1] for row in self.grid]
+            for i in range(N):
+                for j in range(N):
+                    if j>0 and self.grid[i][j]:
+                        shift = 0
+                        for q in range(j):
+                            if self.grid[i][q]==0:
+                                shift += 1
+                        if shift>0:
+                            self.grid[i][j-shift] = self.grid[i][j]
+                            self.grid[i][j] = 0
+                            self.moved = True
+                        if j-shift-1>=0 and self.grid[i][j-shift]==self.grid[i][j-shift-1] and not merged[i][j-shift-1]:
+                            self.grid[i][j-shift-1] *= 2
+                            self.score += self.grid[i][j-shift-1]
+                            self.grid[i][j-shift] = 0
+                            merged[i][j-shift-1] = True
+                            self.moved = True
+            if move=='r':
+                self.grid = [row[::-1] for row in self.grid]
+        
+        elif move in {'u', 'd'}:
+            if move=='d':
+                self.grid = self.grid[::-1]
+            for i in range(N):
+                for j in range(N):
+                    if i>0 and self.grid[i][j]:
+                        shift = 0
+                        for q in range(i):
+                            if self.grid[q][j]==0:
+                                shift += 1
+                        if shift>0:
+                            self.grid[i-shift][j] = self.grid[i][j]
+                            self.grid[i][j] = 0
+                            self.moved = True
+                        if i-shift-1>=0 and self.grid[i-shift-1][j]==self.grid[i-shift][j] and not merged[i-shift-1][j]:
+                            self.grid[i-shift-1][j] *= 2
+                            self.score += self.grid[i-shift-1][j]
+                            self.grid[i-shift][j] = 0
+                            merged[i-shift-1][j] = True
+                            self.moved = True
+            if move=='d':
+                self.grid = self.grid[::-1]
 
-    draw_board()
-    draw_pieces(board_values)
-    
-    if spawn_new:
-        board_values, game_over = new_pieces(board_values)
-        spawn_new = False
-    
-    if direction!='':
-        new_bord, can_move = take_turn(direction, board_values)
-        if can_move:
-            board_values = new_bord
-            spawn_new = True
+    def draw_game(self):
+        """Draw the board with tile and number"""
 
-        direction = ''
-    
-    if game_over:
-        draw_over()
-        if high_score>init_high:
-            file_high_score = open('high_score.txt', 'w')
-            file_high_score.write(f'{high_score}')
-            file_high_score.close()
-            init_high = high_score
-    
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            run = False  
+        # background color
+        background_rect = pygame.Rect(0, 0, self.width, self.width)
+        pygame.draw.rect(self.screen, COLORS['bg'], background_rect, border_bottom_left_radius=8, border_bottom_right_radius=8)
 
-        if event.type==pygame.KEYUP:
-            if event.key==pygame.K_UP:
-                direction = UP
-            elif event.key==pygame.K_DOWN:
-                direction = DOWN
-            elif event.key==pygame.K_LEFT:
-                direction = LEFT
-            elif event.key==pygame.K_RIGHT:
-                direction = RIGHT
-            
-            if game_over:
-                if event.key==pygame.K_RETURN:
-                    board_values = [[0 for _ in range(4)] for _ in range(4)]
-                    spawn_new = True
-                    score = 0
-                    direction = ''
-                    first_time = True
+        # print each cell
+        for i in range(N):
+            for j in range(N):
+                val = self.grid[i][j]
+                # 1. Create rectangle
+                pos_x = j*self.width // N + self.spacecing
+                pos_y = i*self.width // N + self.spacecing
+                w_rect = self.width // N - 2*self.spacecing
+                h_rect = self.width // N - 2*self.spacecing
+                board_rect = pygame.Rect(pos_x, pos_y, w_rect, h_rect)
+                
+                # 2. draw rectangle
+                if val<=2048:
+                    tile_color = COLORS[val]
+                else:
+                    tile_color = COLORS['highest value']
+                pygame.draw.rect(self.screen, tile_color, board_rect, border_radius=5)
 
-    if score>high_score:
-        high_score = score
+                # Print the value on the Tile
+                font = pygame.font.Font('freesansbold.ttf', w_rect-12*len(str(val)))
+                if val<=8:
+                    val_color = COLORS['dark text']
+                elif 8<val<=2048:
+                    val_color = COLORS['light text']
+                else:
+                    val_color = COLORS['other']
+                if val:
+                    text_surface = font.render(str(val), True, val_color)
+                    text_rect = text_surface.get_rect(center=(pos_x + w_rect//2, pos_y + h_rect//2))
+                    self.screen.blit(text_surface, text_rect)
+                    pygame.draw.rect(self.screen, 'black', [pos_x, pos_y, w_rect, h_rect], 1, 5)
 
-    pygame.display.flip()
+    def user_move(self):
+        """Wait for user move"""
 
-pygame.quit()
+        while True:
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    return 'q'
+                if event.type==pygame.KEYDOWN:
+                    if self.is_over:
+                        if event.key==pygame.K_RETURN:
+                            return 'y'
+                        elif event.key==pygame.K_ESCAPE:
+                            return 'q'
+                        else:
+                            continue
+                    else:
+                        if event.key==pygame.K_UP:
+                            return 'u'
+                        elif event.key==pygame.K_DOWN:
+                            return 'd'
+                        elif event.key==pygame.K_LEFT:
+                            return 'l'
+                        elif event.key==pygame.K_RIGHT:
+                            return 'r'
+                        elif event.key==pygame.K_ESCAPE:
+                            return 'q'
+                        else:
+                            continue
+
+    def play(self):
+        """Play the game 2048 (main)"""
+
+        self.new_number(k=2)
+
+        while True:
+            self.draw_score()
+            self.draw_game()
+            pygame.display.flip()
+
+            key = self.user_move()
+
+            if key == 'q':
+                pygame.quit()
+                exit()
+
+            self.make_move(key)
+
+            if not self.moved:
+                continue
+
+            self.new_number()
+            self.moved = False
+
+            if self.is_game_over():
+                if self.high_score > self.initial_high_score:
+                    file_high_score = open('high_score.txt', 'w')
+                    file_high_score.write(f'{self.high_score}')
+                    file_high_score.close()
+
+                self.draw_score()
+                self.draw_game()
+                self.draw_game_over()
+                pygame.display.flip()
+
+                key = self.user_move()
+
+                if key == 'y':
+                    play_2048 = Game_2048()
+                    play_2048.play()
+                else:
+                    pygame.quit()
+                    exit()
+
+            if self.score > self.high_score:
+                self.high_score = self.score
+
+            self.time.tick(60)
+
+if __name__=="__main__":
+    play_2048 = Game_2048()
+    play_2048.play()
