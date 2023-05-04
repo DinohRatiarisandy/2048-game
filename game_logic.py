08deff9e8b056e845e76b513fb66b416f55aa6f7
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2023-05-04 07:43:30 +0300

    logique du 2048 (console mode avec python convention)

--- game_logic.py
+++ game_logic.py
@@ -1,25 +1,36 @@
+"""Module for the main logic of 2048 (CONSOLE MODE)"""
+
 import random
 
-class Game_logic:
+class GameLogic:
+    """The logic of the game (merge two same numbers, move etc.)"""
+
     def __init__(self):
         self.grid = [[0 for _ in range(4)] for _ in range(4)]
         self.moved = False
 
     def show(self):
+        """Print the grid on console"""
+
         return str('\n'.join('\t'.join(map(str, ans)) for ans in self.grid))
-    
+
     def check_game_over(self, grid):
+        """Check if the game if it's over or not"""
+
         for i in range(4):
             for j in range(4):
                 if grid[i][j]==0:
                     return False
-                for r, c in [(i+1, j), (i, j+1)]:
-                    if 0<=r<4 and 0<=c<4 and grid[r][c]==grid[i][j]:
+                for row, col in [(i+1, j), (i, j+1)]:
+                    if 0<=row<4 and 0<=col<4 and grid[row][col]==grid[i][j]:
                         return False
         return True
-    
+
     def new_number(self, k=1):
+        """Add new numbers on the empty tiles in after user's move"""
+
         empty_tiles = []
+
         # get free positions in the grid
         for i in range(4):
             for j in range(4):
@@ -29,14 +40,16 @@ class Game_logic:
         # print 2 or 4 in two positions on the empty tiles
         while k:
             random.shuffle(empty_tiles)
-            x, y = empty_tiles.pop()
+            row, col = empty_tiles.pop()
             if random.random()<0.1:
-                self.grid[x][y] = 4
+                self.grid[row][col] = 4
             else:
-                self.grid[x][y] = 2
+                self.grid[row][col] = 2
             k -= 1
 
     def make_move(self, move):
+        """Actualise the grid after user's move"""
+
         merged = [[False for _ in range(4)] for _ in range(4)]
         self.grid = self.grid.copy()
 
@@ -47,21 +60,22 @@ class Game_logic:
                 for j in range(4):
                     if j>0 and self.grid[i][j]:
                         shift = 0
-                        for q in range(j):
-                            if self.grid[i][q]==0:
+                        for i_backward in range(j):
+                            if self.grid[i][i_backward]==0:
                                 shift += 1
                         if shift>0:
                             self.grid[i][j-shift] = self.grid[i][j]
                             self.grid[i][j] = 0
                             self.moved = True
-                        if j-shift-1>=0 and self.grid[i][j-shift]==self.grid[i][j-shift-1] and not merged[i][j-shift-1]:
+                        if j-shift-1>=0 and self.grid[i][j-shift]==self.grid[i][j-shift-1] \
+                            and not merged[i][j-shift-1]:
                             self.grid[i][j-shift-1] *= 2
                             self.grid[i][j-shift] = 0
                             merged[i][j-shift-1] = True
                             self.moved = True
             if move=='r':
                 self.grid = [row[::-1] for row in self.grid]
-        
+
         elif move in {'u', 'd'}:
             if move=='d':
                 self.grid = self.grid[::-1]
@@ -69,14 +83,15 @@ class Game_logic:
                 for j in range(4):
                     if i>0 and self.grid[i][j]:
                         shift = 0
-                        for q in range(i):
-                            if self.grid[q][j]==0:
+                        for i_backward in range(i):
+                            if self.grid[i_backward][j]==0:
                                 shift += 1
                         if shift>0:
                             self.grid[i-shift][j] = self.grid[i][j]
                             self.grid[i][j] = 0
                             self.moved = True
-                        if i-shift-1>=0 and self.grid[i-shift-1][j]==self.grid[i-shift][j] and not merged[i-shift-1][j]:
+                        if i-shift-1>=0 and self.grid[i-shift-1][j]==self.grid[i-shift][j] \
+                            and not merged[i-shift-1][j]:
                             self.grid[i-shift-1][j] *= 2
                             self.grid[i-shift][j] = 0
                             merged[i-shift-1][j] = True
@@ -85,6 +100,8 @@ class Game_logic:
                 self.grid = self.grid[::-1]
 
     def play(self):
+        """Play the game"""
+
         self.new_number(k=2)
 
         while True:
@@ -111,5 +128,5 @@ class Game_logic:
                     return
 
 if __name__=='__main__':
-    game = Game_logic()
+    game = GameLogic()
     game.play()
